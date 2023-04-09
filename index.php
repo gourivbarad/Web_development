a9acd14dd168cab508b99a00c8096718b3a24dfe
Gouri Vinod Barad <109137073+gourivbarad@users.noreply.github.com>
2023-04-09 21:56:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<html>
+    <head>
+        <title>Food order website-Home pages</title>
+        <link rel="stylesheet" href="admin.css">
+</head>
+<body>
+    <!--menu section starts-->
+    <div class="menu text-center">
+        <div class="wrapper">
+        <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="manage_admin.php">Admin</a></li>
+                <li><a href="manage_category.php">Category</a></li>
+                <li><a href="manage_food.php">Food</a></li>
+                <li><a href="manage_order.php">Order</a></li>
+            </ul>
+        </div>
+
+    </div>
+
+    <!--menu section ends-->
+
+    <!--main content section starts-->
+    <div class="main-content">
+        <div class="wrapper">
+            <div class="font-size text-center">
+                DashBoard
+            </div>
+            <div class="col text-center">
+                <h1>5</h1><br>
+                Categories
+            </div>
+            <div class="col text-center">
+                <h1>5</h1><br>
+                Categories
+            </div>
+            <div class="col text-center">
+                <h1>5</h1><br>
+                Categories
+            </div>
+            <div class="col text-center">
+                <h1>5</h1><br>
+                Categories
+            </div>
+            <div class="col text-center">
+                <h1>5</h1><br>
+                Categories
+            </div>
+            <div class="col text-center">
+                <h1>5</h1><br>
+                Categories
+            </div>
+            <div class="col text-center">
+                <h1>5</h1><br>
+                Categories
+            </div>
+            <div class="col text-center">
+                <h1>5</h1><br>
+                Categories
+            </div>
+                  
+            <div class="clear-fix"></div>              
+        </div>        
+    </div>
+    <!--main content section ends-->
+    
+    <!--footer  section starts-->
+    <div class="footer">
+        <div class="wrapper text-center">
+            <p>2023 All rights reserved, Some Restaurant. Developed by <a href=#>Gouri Vinod Barad</a></p>
+        
+        </div>
+        
+    </div>
+    <!--footer section ends-->
+    
+    
+</body>
+</html>
---
+++ manage_food.php
@@ -0,0 +1,16 @@
+<?php include('menu.php')?>
+
+<!--main content section starts-->
+<div class="main-content">
+        <div class="wrapper">
+            <div class="font-size text-center">
+                Manage food
+            </div>
+            
+                  
+            <div class="clear-fix"></div>              
+        </div>        
+    </div>
+    <!--main content section ends-->
+
+<?php include('footer.php')?>
